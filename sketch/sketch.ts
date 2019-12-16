0fa4f21b82701a37d455f449d1114113ccc5b2c9
Anne-Lie Bäck <a-l.back@outlook.com>
2019-12-16 14:57:57 +0100

    some kind of disco-dots

--- sketch/sketch.ts
+++ sketch/sketch.ts
@@ -16,10 +16,11 @@ function preload() {
  * in the draw function below
  */
 function setup() {
-    createCanvas(900, 550)
-    frameRate(60)
-    noCursor()
-    fullscreen()
+    createCanvas(500, 550);
+    frameRate(60);
+    noCursor();
+    fullscreen();
+
 }
 
 /**
@@ -27,11 +28,88 @@ function setup() {
  * This is a good place to call public funcions of the object
  * you created in the setup function above
  */
+let bgColor = 'black';
+let squareY = 0;
+
 function draw() {
-    background('black');
-    rect(100,100,100,50);
+    background(bgColor);
+
+    fill('red');
+    rectMode(CENTER);
+    rect(mouseX, squareY, 100, 50);
+    
+    let circle1 = new Circle(200, 50, 20, 20);
+    let circle2 = new Circle(50, 300, 50, 50);
+    //circle1.fill();
+    //circle1.fillColor();
+    circle1.draw();
+    circle2.draw();
+/* 
+    ellipse(circleX, mouseY, 60, 60);
+    circleX = circleX + 1;*/
+    squareY = squareY + 1; 
+}
+/* 
+ abstract class Color{
+    
+    public r: number;
+    public g: number;
+    public b: number;
+
+    public constructor(){
+        this.r = random(255);
+        this.g = random(255);
+        this.b = random(255);
+    }
+
+     public fillColor(){
+        return fill(this.r, this.g, this.b);
+    }    
+}  */
+//still trying to solve problem with typescript and classes
+class Circle{
+    
+    public x: number;
+    public y: number;
+    public width: number;
+    public height: number;
+
+    public constructor(x: number, y: number, width:number, height:number){
+        //super();
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        //this.fillColor();
+    }
+
+     public colorRandomizer(){
+
+        const r = random(255);
+        const g = random(255);
+        const b = random(255);
+
+        return fill(r, g, b);
+    }  
+
+    public draw(){
+        this.colorRandomizer();
+        ellipseMode(CENTER);
+        return ellipse(this.x, this.y, this.width, this.height);
+    }  
 }
 
+
+function mousePressed(){
+    if (bgColor === 'black'){
+        bgColor = 'blue';
+    }
+
+    else{
+        bgColor = 'black';
+    }
+    
+}
 /* class Bubble{
     private radius: number;
 } */
