c7cbd057230974342fa6ac98bb85b0636ab2dc11
Anne-Lie Bäck <a-l.back@outlook.com>
2019-12-17 16:22:19 +0100

    square bouncing back and forth

--- sketch/sketch.ts
+++ sketch/sketch.ts
@@ -21,6 +21,7 @@ function setup() {
     noCursor();
     fullscreen();
 
+
 }
 
 /**
@@ -28,26 +29,61 @@ function setup() {
  * This is a good place to call public funcions of the object
  * you created in the setup function above
  */
-let bgColor = 'black';
-let squareY = 0;
+let bgColor:any = 'black';
+let squareY:number = 0;
+let squareSpeed:number = 1
 
 function draw() {
     background(bgColor);
-
-    fill('red');
-    rectMode(CENTER);
-    rect(mouseX, squareY, 100, 50);
-    
-    let circle1 = new Circle(200, 50, 20, 20);
-    let circle2 = new Circle(50, 300, 50, 50);
+    let bubble1 = new Bubble(mouseX, mouseY, 100);
+    let bubble2 = new Bubble(50, 300, 50);
+    let rectangle1 = new Rectangle(mouseX, squareY, 100,50);
     //circle1.fill();
     //circle1.fillColor();
-    circle1.draw();
-    circle2.draw();
+    bubble1.show();
+    bubble2.show();
+    rectangle1.move();
+    rectangle1.show();
+    //rectangle1.move();
 /* 
     ellipse(circleX, mouseY, 60, 60);
     circleX = circleX + 1;*/
-    squareY = squareY + 1; 
+
+
+}
+
+class Rectangle{
+    public x:any;
+    public y:number;
+    public width: number;
+    public height: number;
+
+    public constructor(x:any, y:number, width:number, height:number){
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+
+    public move(){
+        if (squareY > 550){
+            squareSpeed = -1
+        }
+        
+        if (squareY < 0){
+            squareSpeed = + 1
+        }   
+        squareY = squareY + squareSpeed;
+    }
+
+
+    public show(){
+        rectMode(CENTER);
+        fill('red');
+        console.log(this.y);  
+        return rect(this.x, this.y, this.width, this.height);
+         
+    }
 }
 /* 
  abstract class Color{
@@ -67,35 +103,59 @@ function draw() {
     }    
 }  */
 //still trying to solve problem with typescript and classes
-class Circle{
+class Bubble{
     
-    public x: number;
-    public y: number;
-    public width: number;
-    public height: number;
+    public x: any;
+    public y: any;
+    public radius: number;
 
-    public constructor(x: number, y: number, width:number, height:number){
+    public constructor(x: any, y: any, radius:number){
         //super();
         this.x = x;
         this.y = y;
-        this.width = width;
-        this.height = height;
+        this.radius = radius;
         //this.fillColor();
     }
 
-     public colorRandomizer(){
+/*      public colorRandomizer(){
 
         const r = random(255);
         const g = random(255);
         const b = random(255);
 
         return fill(r, g, b);
-    }  
+    }  */ 
 
-    public draw(){
-        this.colorRandomizer();
+/*     public setRandomizedColor(){
+        randomSeed(99)
+        for (let i = 0; i < 100; i++){
+            let r:number = random(0,255);
+            let g:number = random(0,255);
+            let b:number = random(0,255);
+            return fill(r,g,b);
+        }
+    } */
+
+/*      public randomizeColorBubbles(){
+        let colorOfBubbles = ["pink" , "skyblue" , "lightgreen"];
+        return random(colorOfBubbles);
+    } */ 
+
+    public show(){
+        //fill(this.randomizeColorBubbles());
+        //this.setRandomizedColor();
+        //this.colorRandomizer();
+        //fill(this.randomizeColorBubbles());
+        
+         noStroke()
+        if(mouseX<300){
+            fill('pink')
+        }
+        else{
+            fill('lightgreen');
+        } 
         ellipseMode(CENTER);
-        return ellipse(this.x, this.y, this.width, this.height);
+        return circle(this.x, this.y, this.radius);
     }  
 }
 
